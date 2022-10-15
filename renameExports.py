6c4c2b24c633fc55e11a6c7ffa7b35f7f48f5391
Greg Myers <10425307+glmyers@users.noreply.github.com>
2022-10-15 10:38:51 -0400

    Update renameExports.py
    updated logic to be OS independent

--- renameExports.py
+++ renameExports.py
@@ -1,31 +1,30 @@
 #!/usr/bin/env python3
-'''Python script to rename and move Veracross CSV download files from the user's
-Downloads folder to a downloads folder located in the same locations as the
-script itself. The script will make the destination folder if it does not
-already exist. (Designed on a Mac and may require modification for different OS.)
+'''Python script to rename and move Veracross CSV download files from the
+Downloads folder in the user's home directory to a downloads folder located in
+the same locations as the script itself. The script will make the destination
+folder if it does not already exist.
 
 The CSV export files are expected to be downloaded immeadiately before the
 script is run from Veracross into the user's Downloads folder.
 
 Downloads from other dates will not be moved scince the script only looks for
-files ending in 'YYYY-MM-DD.csv' which is the standard Veracross ending.
+files ending in 'YYYY-MM-DD.csv' which is the standard Veracross ending where
+the date is the date of the export.
 '''
 
 # import modules used
 from os import listdir
-from os import path
 from os import makedirs
 from datetime import date
 from shutil import move
-from getpass import getuser
+from pathlib import Path
 
 # Function to rename and move multiple CSV files exported from Veracross
 def vcFiles():
     print()
-    if not path.exists('downloads'):
+    if not Path('downloads').is_dir():
         makedirs('downloads')
-    cUser = getuser()
-    sourceFolder = f'/Users/{cUser}/Downloads'
+    sourceFolder = f'{Path.home()}/Downloads'
     destinationFolder = 'downloads'
     for filename in listdir(sourceFolder):
         if filename[-14:] != f'{date.today()}.csv': continue
@@ -35,7 +34,7 @@ def vcFiles():
         # rename and move files
         move(source, destination)
         print(f'{newName} moved to downloads')
-
+        print()
 
 def main():
     vcFiles()
